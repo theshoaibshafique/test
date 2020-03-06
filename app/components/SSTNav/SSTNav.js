8ae1a6ef06eb5431a96ba6796379366ed1ca0fa6
Joe Lubianski <j.lubianski@surgicalsafety.com>
2020-03-06 10:33:15 -0500

    Remove unused code

--- app/components/SSTNav/SSTNav.js
+++ app/components/SSTNav/SSTNav.js
@@ -2,42 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.scss';
 import DistractionLogo from './img/distraction.svg';
-import CultureSurveyLogo from './img/culture-survey.svg';
-import RoomTrafficLogo from './img/room-traffic.svg';
 
 class SSTNav extends React.Component {
   constructor(props) {
     super(props);
+
   }
 
   render() {
-    let path = window.location.pathname.split('/');
-    let mainPackage;
-    if (path.length > 1) {
-      mainPackage = path[1].replace('-', ' ').replace('/', '');
-    }
-
-    let PackageLogo;
-
-    switch(mainPackage) {
-      case 'distractions':
-        PackageLogo = <img className="Package-Logo" src={DistractionLogo} />;
-        break;
-      case 'culture survey':
-        PackageLogo = <img className="Package-Logo" src={CultureSurveyLogo} />;
-        break;
-      case 'room traffic':
-        PackageLogo = <img className="Package-Logo" src={RoomTrafficLogo} />;
-        break;
-      default:
-        PackageLogo = <span className="dark-green small"><em>Insights</em></span>
-    }
+    const PackageLogo = <div><img className="Package-Logo" src={DistractionLogo} /><span className="dark-green small"><em>Insights</em></span></div>;
 
     return (
       <div>
         <div className="Package-Location center-align">
-          <img className="Package-Logo" src={DistractionLogo} />
-          {PackageLogo}<br />
+          {PackageLogo}
         </div>
         <ul className="dark-blue">
           {(this.props.emmAccess) &&
