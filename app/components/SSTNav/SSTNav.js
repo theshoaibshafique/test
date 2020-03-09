5e63629254a0a1203e822418155af54a5ec6ebec
Joe Lubianski <j.lubianski@surgicalsafety.com>
2020-03-09 12:08:49 -0400

    Style updates for left nav

--- app/components/SSTNav/SSTNav.js
+++ app/components/SSTNav/SSTNav.js
@@ -15,7 +15,7 @@ class SSTNav extends React.Component {
           <div><img className="Package-Logo" src={logo} /></div>
         </div>
         <ul className="dark-blue">
-          <li><Link to="/maindashboard" className='text-link'>Dashboard</Link></li>
+          <li className="link-border"><Link to="/maindashboard" className='text-link'>Dashboard</Link></li>
           {(this.props.emmAccess) &&
             <li><Link to="/emmcases" className='text-link'>eM&M Cases</Link></li>
           }
@@ -23,13 +23,16 @@ class SSTNav extends React.Component {
             <li>Efficiency</li>
             <li>Surgical Safety Checklist</li>
           */}
-          {(this.props.emmRequestAccess) && 
-            <li><Link to="/requestemm" className='text-link'>Request for eM&M</Link></li>
-          }
-          {(this.props.userManagementAccess) &&
-            <li><Link to="/usermanagement" className='text-link'>User Management</Link></li> 
-          }
-          <li><Link to="/my-profile" className='text-link'>My Profile</Link></li>
+
+          <div className="bottom-left">
+            {(this.props.emmRequestAccess) && 
+              <li className="link-border"><Link to="/requestemm" className='text-link'>Request for eM&M</Link></li>
+            }
+            {(this.props.userManagementAccess) &&
+              <li className="link-border"><Link to="/usermanagement" className='text-link'>User Management</Link></li> 
+            }
+            <li className="link-border"><Link to="/my-profile" className='text-link'>My Profile</Link></li>
+          </div>
         </ul>
       </div>
     );
