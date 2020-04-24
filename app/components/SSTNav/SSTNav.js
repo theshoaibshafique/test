294f43d47320de76611f53619483956d64e41f2b
Adam Lee <adamjlee96@gmail.com>
2020-04-24 11:46:38 -0400

    fix sstnav display bug

--- app/components/SSTNav/SSTNav.js
+++ app/components/SSTNav/SSTNav.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './style.scss';
 import logo from './img/SST-Product_Insights_sketch.png';
-import { List, ListItem, Collapse } from '@material-ui/core';
+import { List, ListItem, Collapse, Grid } from '@material-ui/core';
 import IconExpandLess from '@material-ui/icons/ExpandLess'
 import IconExpandMore from '@material-ui/icons/ExpandMore'
 class SSTNav extends React.Component {
@@ -33,54 +33,56 @@ class SSTNav extends React.Component {
 
   render() {
     return (
-      <div className="sstnav dark-blue" >
-        <List disablePadding >
-          <ListItem className="Package-Location center-align" disableGutters>
-            <img className="Package-Logo" src={logo} />
-          </ListItem>
+      <Grid container spacing={0} className="sstnav dark-blue" style={{height:"100%"}}>
+        <Grid item xs={12}>
+          <List disablePadding >
+            <ListItem className="Package-Location center-align" disableGutters>
+              <img className="Package-Logo" src={logo} />
+            </ListItem>
 
-          <ListItem disableGutters><NavLink to="/maindashboard" className='text-link '>Dashboard</NavLink></ListItem>
-          {(this.props.emmAccess) &&
-            <ListItem disableGutters><NavLink to="/emmcases" className='text-link' >eM&M Cases</NavLink></ListItem>
-          }
-          {(this.props.emmPublishAccess) &&
-            <ListItem disableGutters><NavLink to="/emmpublish" className='text-link' >eM&M Publisher</NavLink></ListItem>
-          }
-          {/* 
+            <ListItem disableGutters><NavLink to="/maindashboard" className='text-link '>Dashboard</NavLink></ListItem>
+            {(this.props.emmAccess) &&
+              <ListItem disableGutters><NavLink to="/emmcases" className='text-link' >eM&M Cases</NavLink></ListItem>
+            }
+            {(this.props.emmPublishAccess) &&
+              <ListItem disableGutters><NavLink to="/emmpublish" className='text-link' >eM&M Publisher</NavLink></ListItem>
+            }
+            {/* 
             <li>Efficiency</li>
             <li>Surgical Safety Checklist</li>
           */}
 
-          <ListItem disableGutters>
-            <NavLink to="/sschecklist" className='text-link'>
-              <div>Surgical Safety Checklist</div>
-            </NavLink>
-            <div style={{ marginRight: 16, position: 'absolute', right:0}} onClick={() => this.toggleSSC()}>
-              {this.state.isSSCOpen ? <IconExpandLess /> : <IconExpandMore />}
-            </div>
-          </ListItem>
+            <ListItem disableGutters>
+              <NavLink to="/sschecklist" className='text-link'>
+                <div>Surgical Safety Checklist</div>
+              </NavLink>
+              <div style={{ marginRight: 16, position: 'absolute', right: 0 }} onClick={() => this.toggleSSC()}>
+                {this.state.isSSCOpen ? <IconExpandLess /> : <IconExpandMore />}
+              </div>
+            </ListItem>
 
-          <Collapse in={this.state.isSSCOpen} timeout="auto" unmountOnExit>
-            <ListItem disableGutters><NavLink to="/checklistScore" className='text-link sub-item' >Checklist Score</NavLink></ListItem>
-            <ListItem disableGutters><NavLink to="/engagementScore" className='text-link sub-item' >Engagement Score</NavLink></ListItem>
-            <ListItem disableGutters><NavLink to="/qualityScore" className='text-link sub-item' >Quality Score</NavLink></ListItem>
-          </Collapse>
-        </List>
+            <Collapse in={this.state.isSSCOpen} timeout="auto" unmountOnExit>
+              <ListItem disableGutters><NavLink to="/checklistScore" className='text-link sub-item' >Checklist Score</NavLink></ListItem>
+              <ListItem disableGutters><NavLink to="/engagementScore" className='text-link sub-item' >Engagement Score</NavLink></ListItem>
+              <ListItem disableGutters><NavLink to="/qualityScore" className='text-link sub-item' >Quality Score</NavLink></ListItem>
+            </Collapse>
+          </List>
+        </Grid>
+        <Grid item xs={12} style={{display:'flex', alignItems:'flex-end'}}>
+          <List disablePadding >
+            {(this.props.emmRequestAccess) &&
+              <ListItem disableGutters><NavLink to="/requestemm" className='text-link' >Request for eM&M</NavLink></ListItem>
+            }
+            {(this.props.userManagementAccess) &&
+              <ListItem disableGutters><NavLink to="/usermanagement" className='text-link' >User Management</NavLink></ListItem>
+            }
+            <ListItem disableGutters><NavLink to="/my-profile" className='text-link' >My Profile</NavLink></ListItem>
 
-        <List disablePadding className="bottom-left">
-          {(this.props.emmRequestAccess) &&
-            <ListItem disableGutters><NavLink to="/requestemm" className='text-link' >Request for eM&M</NavLink></ListItem>
-          }
-          {(this.props.userManagementAccess) &&
-            <ListItem  disableGutters><NavLink to="/usermanagement" className='text-link' >User Management</NavLink></ListItem>
-          }
-          <ListItem disableGutters><NavLink to="/my-profile" className='text-link' >My Profile</NavLink></ListItem>
+            <ListItem disableGutters style={{ marginBottom: 20 }}><NavLink to="/" className='text-link' isActive={() => false} >{this.props.userLogin}</NavLink></ListItem>
+          </List>
+        </Grid>
 
-          <ListItem disableGutters style={{marginBottom:20}}><NavLink to="/" className='text-link' isActive={() => false} >{this.props.userLogin}</NavLink></ListItem>
-        </List>
-
-
-      </div>
+      </Grid>
     );
   }
 }
