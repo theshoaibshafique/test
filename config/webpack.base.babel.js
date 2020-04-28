f8add68e2b84f6d9e3bbdaebd3e7cac70563582b
Derek Lo <d.lo@surgicalsafety.com>
2020-04-28 15:08:28 -0400

    update the build commands that will take the appropriate env files for building env

--- config/webpack.base.babel.js
+++ config/webpack.base.babel.js
@@ -6,7 +6,9 @@ const path = require('path');
 const webpack = require('webpack');
 const dotenv = require('dotenv');
 process.noDeprecation = true;
-const env = dotenv.config().parsed;
+const env = dotenv.config({
+  path: '.env.' + process.env.NODE_ENV
+}).parsed;
 const envKeys = Object.keys(env).reduce((prev, next) => {
   prev[`process.env.${next}`] = JSON.stringify(env[next]);
   return prev;
