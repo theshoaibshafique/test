a00bc6d89bd033794d5fcf703c95088fd57349ce
Derek Lo <d.lo@surgicalsafety.com>
2020-05-13 12:09:45 -0400

    EMM overview grid system

--- app/containers/EMMReports/EMMOverview/EMMOverview.js
+++ app/containers/EMMReports/EMMOverview/EMMOverview.js
@@ -15,13 +15,32 @@ export default class EMMOverview extends React.PureComponent { // eslint-disable
       <div className="Emm-Reports-Overview">
         <Grid container spacing={3}>
           <Grid item xs={4}>
-            <Paper className="Emm-Paper">xs=12</Paper>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Paper className="Emm-Paper">xs=12</Paper>
+              </Grid>
+              <Grid item xs={12}>
+                <Paper className="Emm-Paper">
+                  CASE INFORMATION
+                </Paper>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item xs={4}>
-            <Paper className="Emm-Paper">xs=12</Paper>
-          </Grid>
-          <Grid item xs={4}>
-            <Paper className="Emm-Paper">xs=12</Paper>
+          <Grid item xs={8}>
+            <Grid container spacing={3}>
+              <Grid item xs={6}>
+                <Paper className="Emm-Paper">xs=12</Paper>
+              </Grid>
+              <Grid item xs={6}>
+                <Paper className="Emm-Paper">xs=12</Paper>
+              </Grid>
+              <Grid item xs={12}>
+                <Paper className="Emm-Paper">SSC</Paper>
+              </Grid>
+              <Grid item xs={12}>
+                <Paper className="Emm-Paper">Phases Of Interest</Paper>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </div>
