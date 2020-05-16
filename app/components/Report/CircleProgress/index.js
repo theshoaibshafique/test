af911e845d4322777eb21c2daef8ff095ef259e8
Derek Lo <d.lo@surgicalsafety.com>
2020-05-15 20:30:12 -0400

    added circle progress component

---
+++ app/components/Report/CircleProgress/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { makeStyles, CircularProgress } from '@material-ui/core';
+
+import './style.scss';
+
+const useStylesCircleProgress = makeStyles({
+    root: {
+      textAlign: 'center',
+      color: '#000000',
+      '& h3' : {
+        fontSize: '18px',
+        fontWeight: 'bold',
+        opacity: 0.8
+      },
+      '& h5' : {
+        fontSize: '42px',
+        opacity: 0.8,
+        fontWeight: 'normal',
+        position: 'absolute',
+        width: '100%',
+        margin: 0,
+        top: 55,
+        '& sup' : {
+          fontSize: '14px',
+          top: '-1.5rem'
+        }
+      }
+    },
+    circlewrapper: {
+      position: 'relative',
+    },
+    topcircle: {
+      color: '#F3F3F3',
+    },
+    bottomcircle: {
+      color: props => props.color,
+      animationDuration: '550ms',
+      position: 'absolute',
+      left: 0,
+    },
+  });
+
+export default function CircleProgress(props) {
+  const classes = useStylesCircleProgress(props);
+
+  return(
+    <div className={classes.root}>
+      <h3>{props.title}</h3>
+      <div className={classes.circlewrapper}>
+        <CircularProgress
+          variant="static"
+          value={100}
+          className={classes.topcircle}
+          size={175}
+          thickness={5}
+        />
+        <CircularProgress
+          variant="static"
+          className={classes.bottomcircle}
+          size={175}
+          thickness={5}
+          {...props}
+        />
+        <h5>{props.value}<sup>%</sup></h5>
+      </div>
+    </div>
+  )
+}
--- app/containers/EMMReports/EMMOverview/EMMOverview.js
+++ app/containers/EMMReports/EMMOverview/EMMOverview.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './style.scss';
-import { Grid, Paper, Card, CardContent } from '@material-ui/core';
-import ReportScore from '../../../components/Report/ReportScore/ReportScore';
+import { Grid, Paper } from '@material-ui/core';
+import ReportScore from '../../../components/Report/ReportScore';
+import CircleProgress from '../../../components/Report/CircleProgress';
 
 export default class EMMOverview extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
@@ -45,13 +46,28 @@ export default class EMMOverview extends React.PureComponent { // eslint-disable
               <Grid item xs={6}>
                 <Paper className="Emm-Paper">
                   <ReportScore
-                    pushUrl={this.props.pushUrl}
                     title="Number of Events"
                     score="7" />
                 </Paper>
               </Grid>
               <Grid item xs={12}>
-                <Paper className="Emm-Paper">SSC</Paper>
+                <Paper className="Emm-Paper">
+                  <CircleProgress
+                    title="Checklist Score"
+                    color="#A7E5FD"
+                    value={67}
+                  />
+                  <CircleProgress
+                    title="Engagement Score"
+                    color="#97E7B3"
+                    value={55}
+                  />
+                  <CircleProgress
+                    title="Quality Score"
+                    color="#CFB9E4"
+                    value={83}
+                  />
+                </Paper>
               </Grid>
               <Grid item xs={12}>
                 <Paper className="Emm-Paper">Phases Of Interest</Paper>
