351830f985f50c38fde5a037741262c56b4fad06
Adam Lee <a.lee@surgicalsafety.com>
2020-06-09 16:34:17 -0400

    https://surgicalsafety.atlassian.net/browse/INSSC-151 Group permissions and add loading spinner

--- app/containers/MainLayout/MainLayout.js
+++ app/containers/MainLayout/MainLayout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.scss';
 import { Helmet } from 'react-helmet';
 import { Switch, Route } from 'react-router-dom';
-
+import axios from 'axios';
 import MainDashboard from 'containers/MainDashboard/Loadable';
 import EMMCases from 'containers/EMMCases/Loadable';
 import EMMPublish from 'containers/EMMPublish/Loadable';
@@ -21,6 +21,7 @@ import AzureLogin from 'components/AzureLogin';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Hidden from '@material-ui/core/Hidden';
 import Drawer from '@material-ui/core/Drawer';
+import globalFunctions from '../../utils/global-functions';
 
 export default class MainLayout extends React.PureComponent {
   constructor(props) {
@@ -32,7 +33,7 @@ export default class MainLayout extends React.PureComponent {
       emmAccess: false,
       emmRequestAccess: false,
       emmPublishAccess: false,
-      sscAccess:false,
+      sscAccess: false,
       isLoading: true
     }
 
@@ -44,11 +45,7 @@ export default class MainLayout extends React.PureComponent {
       userLoggedIn: true
     });
 
-    this.getUserManagementAccess();
-    this.getEMMRequestAccess();
-    this.getEMMAccess();
-    this.getEMMPublishAccess();
-    this.getSSCRequestAccess()
+    this.getPageAccess();
   };
 
   redirect() {
@@ -57,144 +54,89 @@ export default class MainLayout extends React.PureComponent {
     });
   };
 
+  getPageAccess() {
+    Promise.all([
+      this.getUserManagementAccess(),
+      this.getEMMRequestAccess(),
+      this.getEMMAccess(),
+      this.getEMMPublishAccess(),
+      this.getSSCRequestAccess()].map(function (e) {
+        return e.then(function(result){
+          return result && result.data;
+        }).catch(function () {
+          return false;
+        })
+      })).then(([userManagementAccess,emmRequestAccess,emmAccess,emmPublishAccess,sscAccess]) => {
+        this.setState({
+          userManagementAccess,emmRequestAccess,emmAccess,emmPublishAccess,sscAccess,isLoading:false
+        })
+      }).catch(function (results) {
+        // console.error(results);
+      });
+  }
+
   getUserManagementAccess() {
-    fetch(process.env.USERMANAGEMENTACCESS_API, {
-      method: 'get',
-      headers: {
-        'Authorization': 'Bearer ' + this.props.userToken,
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => {
-      if (response.status === 200) {
-        response.json().then((result) => {
-          if (result) {
-            this.setState ({ userManagementAccess: true })
-          }
-        });
-      }
-    })
+    return globalFunctions.axiosFetch(process.env.USERMANAGEMENTACCESS_API, 'get', this.props.userToken);
   };
 
   getEMMAccess() {
-    fetch(process.env.EMMACCESS_API, {
-      method: 'get',
-      headers: {
-        'Authorization': 'Bearer ' + this.props.userToken,
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => {
-      if (response.status === 200) {
-        response.json().then((result) => {
-          if (result) {
-            this.setState ({ emmAccess: true })
-          }
-        });
-      }
-    })
+    return globalFunctions.axiosFetch(process.env.EMMACCESS_API, 'get', this.props.userToken);
   };
 
   getEMMPublishAccess() {
-    fetch(process.env.EMMPUBLISHACCESS_API, {
-      method: 'get',
-      headers: {
-        'Authorization': 'Bearer ' + this.props.userToken,
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => {
-      if (response.status === 200) {
-        response.json().then((result) => {
-          if (result) {
-            this.setState ({ emmPublishAccess: true })
-          }
-        });
-      }
-    })
+    return globalFunctions.axiosFetch(process.env.EMMPUBLISHACCESS_API, 'get', this.props.userToken);
   };
-
   getEMMRequestAccess() {
-    fetch(process.env.EMMREQUESTACCESS_API, {
-      method: 'get',
-      headers: {
-        'Authorization': 'Bearer ' + this.props.userToken,
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => {
-      if (response.status === 200) {
-        response.json().then((result) => {
-          if (result) {
-            this.setState ({ emmRequestAccess: true })
-          }
-        });
-      }
-    })
+    return globalFunctions.axiosFetch(process.env.EMMREQUESTACCESS_API, 'get', this.props.userToken);
   };
   getSSCRequestAccess() {
-    fetch(process.env.SSC_ACCESS_API, {
-      method: 'get',
-      headers: {
-        'Authorization': 'Bearer ' + this.props.userToken,
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => {
-      if (response.status === 200) {
-        response.json().then((result) => {
-          if (result) {
-            this.setState ({ sscAccess: true })
-          }
-        });
-      }
-    })
+    return globalFunctions.axiosFetch(process.env.SSC_ACCESS_API, 'get', this.props.userToken);
   };
 
   getContainer() {
     if (!this.state.authenticated) {
       return <Switch>
-                <Route path="/my-profile" component={MyProfile}/>
-                <NoAccess />
-             </Switch>
+        <Route path="/my-profile" component={MyProfile} />
+        <NoAccess />
+      </Switch>
     }
 
     if (this.state.userLoggedIn) {
       return <Switch>
-              <Route path="/dashboard" component={MainDashboard}/> }/>
+        <Route path="/dashboard" component={MainDashboard} /> }/>
               {(this.state.emmAccess) &&
-                  <Route path="/emmcases" component={EMMCases}/>
-              }
-              {(this.state.emmPublishAccess) &&
-                  <Route path="/emmpublish" component={EMMPublish}/>
-              }
-              {(this.state.emmAccess) &&
-                  <Route path="/emm/:requestid" component={EMM} />
-              }
-              {(this.state.emmAccess) &&
-                  <Route path="/emmreport/:requestid" component={EMMReport} />
-              }
-              {(this.state.emmRequestAccess) &&
-                  <Route path="/requestemm" component={RequestEMM}/>
-              }
-              {(this.state.userManagementAccess) &&
-                  <Route path="/usermanagement" component={UserManagement}/>
-              }
-              {(this.state.sscAccess) &&
-                  <Route path="/sschecklist" render={(props) => <SSChecklist {...props} reportType={"SurgicalSafetyChecklistReport"} />}/>
-              }
-              {(this.state.sscAccess) &&
-                  <Route path="/complianceScore" render={(props) => <SSChecklist {...props} reportType={"ComplianceScoreReport"} />}/>
-              }
-              {(this.state.sscAccess) &&
-                  <Route path="/engagementScore" render={(props) => <SSChecklist {...props} reportType={"EngagementScoreReport"} />}/>
-              }
-              {(this.state.sscAccess) &&
-                  <Route path="/qualityScore" render={(props) => <SSChecklist {...props} reportType={"QualityScoreReport"} />}/>
-              }
-              <Route path="/my-profile" component={MyProfile}/>
-              <Route path="" component={NotFoundPage}/>
-            </Switch>
+          <Route path="/emmcases" component={EMMCases} />
+        }
+        {(this.state.emmPublishAccess) &&
+          <Route path="/emmpublish" component={EMMPublish} />
+        }
+        {(this.state.emmAccess) &&
+          <Route path="/emm/:requestid" component={EMM} />
+        }
+        {(this.state.emmAccess) &&
+          <Route path="/emmreport/:requestid" component={EMMReport} />
+        }
+        {(this.state.emmRequestAccess) &&
+          <Route path="/requestemm" component={RequestEMM} />
+        }
+        {(this.state.userManagementAccess) &&
+          <Route path="/usermanagement" component={UserManagement} />
+        }
+        {(this.state.sscAccess) &&
+          <Route path="/sschecklist" render={(props) => <SSChecklist {...props} reportType={"SurgicalSafetyChecklistReport"} />} />
+        }
+        {(this.state.sscAccess) &&
+          <Route path="/complianceScore" render={(props) => <SSChecklist {...props} reportType={"ComplianceScoreReport"} />} />
+        }
+        {(this.state.sscAccess) &&
+          <Route path="/engagementScore" render={(props) => <SSChecklist {...props} reportType={"EngagementScoreReport"} />} />
+        }
+        {(this.state.sscAccess) &&
+          <Route path="/qualityScore" render={(props) => <SSChecklist {...props} reportType={"QualityScoreReport"} />} />
+        }
+        <Route path="/my-profile" component={MyProfile} />
+        <Route path="" component={NotFoundPage} />
+      </Switch>
     } else {
       return ''
     }
@@ -202,49 +144,50 @@ export default class MainLayout extends React.PureComponent {
 
   render() {
     return (
-        <div className="app-wrapper">
-          <CssBaseline />
-          <Helmet
-            titleTemplate="%s - SST Insights"
-            defaultTitle="SST Insights"
-          >
-            <meta name="description" content="SST Insights web portal" />
-          </Helmet>
+      <div className="app-wrapper">
+        <CssBaseline />
+        <Helmet
+          titleTemplate="%s - SST Insights"
+          defaultTitle="SST Insights"
+        >
+          <meta name="description" content="SST Insights web portal" />
+        </Helmet>
 
-          <div className="APP-MAIN-WRAPPER">
-            {(this.props.emmReportID) &&
-              <div className="EMM-Report-Overlay">
-                <EMMReports />
-              </div>
-            }
-            <nav className={"MAIN-NAVIGATION " + (this.props.emmReportID && 'hidden')}>
-              <Hidden xsDown implementation="css">
-                <Drawer
-                  variant="permanent"
-                  open
-                  >
-                  <SSTNav
-                    userManagementAccess={this.state.userManagementAccess}
-                    emmRequestAccess={this.state.emmRequestAccess}
-                    emmAccess={this.state.emmAccess}
-                    emmPublishAccess={this.state.emmPublishAccess}
-                    sscAccess={this.state.sscAccess}
-                    pathname={this.props.location.pathname}
-                    logoutRef={this.logoutRef}
-                    userLogin={<AzureLogin
-                      resourcesGathered={() => this.resourcesGathered()}
-                      redirect={() => this.redirect()}
-                      logoutRef={this.logoutRef}
-                    />}
-                  />
-                </Drawer>
-              </Hidden>
-            </nav>
-            <div className="inline overflow-y Content-Wrapper">
-              {this.getContainer()}
+        <div className="APP-MAIN-WRAPPER">
+          {(this.props.emmReportID) &&
+            <div className="EMM-Report-Overlay">
+              <EMMReports />
             </div>
+          }
+          <nav className={"MAIN-NAVIGATION " + (this.props.emmReportID && 'hidden')}>
+            <Hidden xsDown implementation="css">
+              <Drawer
+                variant="permanent"
+                open
+              >
+                <SSTNav
+                  userManagementAccess={this.state.userManagementAccess}
+                  emmRequestAccess={this.state.emmRequestAccess}
+                  emmAccess={this.state.emmAccess}
+                  emmPublishAccess={this.state.emmPublishAccess}
+                  sscAccess={this.state.sscAccess}
+                  pathname={this.props.location.pathname}
+                  logoutRef={this.logoutRef}
+                  isLoading={this.state.isLoading}
+                  userLogin={<AzureLogin
+                    resourcesGathered={() => this.resourcesGathered()}
+                    redirect={() => this.redirect()}
+                    logoutRef={this.logoutRef}
+                  />}
+                />
+              </Drawer>
+            </Hidden>
+          </nav>
+          <div className="inline overflow-y Content-Wrapper">
+            {this.getContainer()}
           </div>
         </div>
+      </div>
     );
   }
 }
