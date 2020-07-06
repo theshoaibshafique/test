0c4efad9f114c9dd7f2c0f17041777b9e8085c39
Derek Lo <d.lo@surgicalsafety.com>
2020-07-06 10:44:20 -0400

    style update when ss checklist has perfect score

--- app/containers/EMMReports/EMMPhaseAnalysis/EMMPhaseVideoContainer/ChecklistAnalysis.js
+++ app/containers/EMMReports/EMMPhaseAnalysis/EMMPhaseVideoContainer/ChecklistAnalysis.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { Grid, Paper } from '@material-ui/core';
 
 const CheckListData = (props) => {
-  const { data, data : { dataPoints } } = props;
+  const { data, data : { dataPoints }, isPeopleEqual } = props;
 
   const getCheckListData = () => {
     if (data.title == "Missed Items") {
-      return <div className="checklist-data-list main-text">
+      return <div className="checklist-data-list">
                 {
+                  (dataPoints.length == 0) ?
+                    <div className="no-missed-item">No Missed Item</div>
+                  :
                   dataPoints.map((dataPoint, index) => {
-                    return <div key={`missedItem${index}`}>{index + 1}. <span>{dataPoint.valueX}</span></div>
+                    return <div className="main-text" key={`missedItem${index}`}>{index + 1}. <span>{dataPoint.valueX}</span></div>
                   })
                 }
               </div>
     } else {
       return <div
                 className="checklist-data-value"
-                style={(data.title == "Phase") ? {color: '#004F6E'} : {}}
+                style={(data.title == "Phase" || isPeopleEqual) ? {color: '#004F6E'} : {}}
               >
                 {dataPoints[0].valueX}</div>
     }
@@ -40,6 +43,7 @@ const ChecklistAnalysis = (props) => {
           const title = checklist.title;
           const subTitle = checklist.subTitle
           const enhancedMMData = checklist.enhancedMMData;
+          const isPeopleEqual = enhancedMMData[1].dataPoints[0].valueX == enhancedMMData[2].dataPoints[0].valueX;
           return <Paper
                   key={`checklistData${index}`}
                   className="checklist-data">
@@ -48,8 +52,8 @@ const ChecklistAnalysis = (props) => {
                   <Grid container spacing={0}>
                     <Grid item xs={6}>
                       <CheckListData data={enhancedMMData[0]} />
-                      <CheckListData data={enhancedMMData[1]} />
-                      <CheckListData data={enhancedMMData[2]} />
+                      <CheckListData data={enhancedMMData[1]} isPeopleEqual={isPeopleEqual}/>
+                      <CheckListData data={enhancedMMData[2]} isPeopleEqual={isPeopleEqual}/>
                     </Grid>
                     <Grid item xs={6}>
                       <CheckListData data={enhancedMMData[3]} />
