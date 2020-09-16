97449e9fb1232e6336d551b1eb119761b875f1c1
Adam Lee <a.lee@surgicalsafety.com>
2020-09-16 16:41:01 -0400

    https://surgicalsafety.atlassian.net/browse/INGEN-22 fix barchart perfect

--- app/components/Report/BarChart/BarChart.js
+++ app/components/Report/BarChart/BarChart.js
@@ -229,7 +229,7 @@ export default class BarChart extends React.PureComponent {
               this.props.body && this.props.subTitle
                 ? <div><div className="no-data">{this.props.body}</div> <div className="no-data-subtitle">{this.props.subTitle}</div></div>
                 : this.props.body || this.props.description ?
-                  <div className="display-text">{this.props.description}</div>
+                  <div className="display-text">{this.props.body || this.props.description}</div>
                   : <C3Chart className={this.state.chartID} ref={this.chartRef} {...this.state.chartData} />}
           </Grid>
           <Grid item xs>
