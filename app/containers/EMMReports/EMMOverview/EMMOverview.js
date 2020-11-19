34a873827cb1980a0720ab0852cf8fff274d356e
Adam Lee <a.lee@surgicalsafety.com>
2020-11-19 15:01:41 -0500

    Clean up chart layout for HL7 with Hardcoded data

--- app/containers/EMMReports/EMMOverview/EMMOverview.js
+++ app/containers/EMMReports/EMMOverview/EMMOverview.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './style.scss';
 import { Grid, Paper, Tooltip, withStyles } from '@material-ui/core';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
-import { mdiVolumeVibrate,mdiDivingScubaTank,mdiResistor,mdiThermometerLow,mdiTrendingUp,mdiWater,mdiAlertRhombusOutline ,mdiSpeedometer } from '@mdi/js';
-import { EMM_DISTRACTION_TOOLTIP, EMM_TECHNICAL_TOOLTIP, EMM_ADVERSEEVENT_TOOLTIP } from '../../../constants'
+import { mdiVolumeVibrate, mdiDivingScubaTank, mdiResistor, mdiThermometerLow, mdiTrendingUp, mdiWater, mdiAlertRhombusOutline, mdiSpeedometer, mdiSpeedometerMedium, mdiSpeedometerSlow } from '@mdi/js';
+import { EMM_DISTRACTION_TOOLTIP, EMM_TECHNICAL_TOOLTIP, EMM_ADVERSEEVENT_TOOLTIP, HL7_DATA } from '../../../constants'
 import ChecklistStatus from './ChecklistStatus';
 import PhasesOfInterest from './PhasesOfInterest';
 import CaseInformation from './CaseInformation';
 import SurgicalSafetyChecklist from './SurgicalSafetyChecklist';
 import globalFuncs from '../../../utils/global-functions';
 import Icon from '@mdi/react'
+import HorizontalBarChart from './HorizontalBarChart';
 
 const LightTooltip = withStyles((theme) => ({
   tooltip: {
@@ -17,8 +18,9 @@ const LightTooltip = withStyles((theme) => ({
     padding: '16px',
     fontSize: '14px',
     lineHeight: '19px',
-    font: 'Noto Sans'
-  }
+    font: 'Noto Sans',
+    maxWidth:500
+  },
 }))(Tooltip);
 
 export default class EMMOverview extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
@@ -39,9 +41,24 @@ export default class EMMOverview extends React.PureComponent { // eslint-disable
       });
   }
 
+  getSpeedometer(speed){
+    switch (speed) {
+      case 'slow':
+        return <Icon color="#FF4D4D" path={mdiSpeedometerSlow} size={'20px'} />;
+      case 'medium':
+        return <Icon color="#FFB71B" path={mdiSpeedometerMedium} size={'20px'} />;
+      default:
+        return <Icon color="#009483" path={mdiSpeedometer} size={'20px'} />;
+    }
+  }
+
   render() {
     const { tabShowing } = this.props;
     const { emmReportData, emmReportData: { distractionScore, technicalPerformanceScore, adverseEventRate, checklistScore, checklists, phasesOfInterest }, specialties, complications } = this.props;
+    const {hypotension, hypothermia, hypoxia} = HL7_DATA;
+    const hasHypotension = hypotension && hypotension.dataPoints && hypotension.dataPoints.length;
+    const hasHypothermia = hypothermia && hypothermia.dataPoints && hypothermia.dataPoints.length;
+    const hasHypoxia = hypoxia && hypoxia.dataPoints && hypoxia.dataPoints.length;
     const adverseEventRateTitle = adverseEventRate.dataPoints[0].valueX.substr(0, adverseEventRate.dataPoints[0].valueX.length - 3)
     return (
       <div
@@ -61,7 +78,7 @@ export default class EMMOverview extends React.PureComponent { // eslint-disable
                   </div>
                   <div>
                     <span className="EMM-Score">{distractionScore.dataPoints[0].valueX}</span>
-                    <span className="EMM-Score-Icon" style={{backgroundColor: 'rgba(167, 229, 253,.6)'}}><Icon color="#50CBFB" style={{marginTop:4}} path={mdiVolumeVibrate} size={'40px'} /></span>
+                    <span className="EMM-Score-Icon" style={{ backgroundColor: 'rgba(167, 229, 253,.6)' }}><Icon color="#50CBFB" style={{ marginTop: 4 }} path={mdiVolumeVibrate} size={'40px'} /></span>
                   </div>
                 </Paper>
               </Grid>
@@ -76,10 +93,10 @@ export default class EMMOverview extends React.PureComponent { // eslint-disable
                   <div>
                     <span className="EMM-Score">{
                       (parseInt(technicalPerformanceScore.dataPoints[0].valueX) < 0) ?
-                        <span className="performance-unavailable">Unavailable for this case</span>
+                        <span className="performance-unavailable">Not Available</span>
                         : technicalPerformanceScore.dataPoints[0].valueX
                     }</span>
-                    <span className="EMM-Score-Icon" style={{backgroundColor: 'rgba(151, 231, 179,.6)'}}><Icon color="#6EDE95" path={mdiTrendingUp} size={'32px'} /></span>
+                    <span className="EMM-Score-Icon" style={{ backgroundColor: 'rgba(151, 231, 179,.6)' }}><Icon color="#6EDE95" path={mdiTrendingUp} size={'32px'} /></span>
                   </div>
 
                 </Paper>
@@ -94,66 +111,75 @@ export default class EMMOverview extends React.PureComponent { // eslint-disable
                   </div>
                   <div>
                     <span className="EMM-Score">{adverseEventRateTitle}<span style={{ fontSize: '26px' }}>/hr</span></span>
-                    <span className="EMM-Score-Icon" style={{backgroundColor: 'rgba(255, 219, 140,.6)'}}><Icon color="#FFC74D" path={mdiAlertRhombusOutline } size={'32px'} /></span>
+                    <span className="EMM-Score-Icon" style={{ backgroundColor: 'rgba(255, 219, 140,.6)' }}><Icon color="#FFC74D" path={mdiAlertRhombusOutline} size={'32px'} /></span>
                   </div>
                 </Paper>
               </Grid>
               {/* HL7 KPIs */}
-              <Grid item xs={12}>
+              <Grid item xs={12} className="HL7">
                 <Paper className="Emm-Paper Score">
                   <Grid container spacing={9}>
-                    <Grid item xs={4}>
+                    <Grid item xs={4} style={{ position: 'relative' }}>
+                      <LightTooltip interactive arrow title={hypotension.body} placement="top" fontSize="small">
+                        <span className="Score-Speedometer">{this.getSpeedometer(hypotension.description)}</span>
+                      </LightTooltip>
                       <div className="Section-Title">
-                        Hypotension
-                        <LightTooltip interactive arrow title={EMM_DISTRACTION_TOOLTIP} placement="top" fontSize="small">
+                        {hypotension.title}
+                        <LightTooltip interactive arrow title={<HorizontalBarChart dataPoints={hypotension.dataPoints} title={hypotension.subTitle}/>} placement="top" fontSize="small">
                           <InfoOutlinedIcon style={{ fontSize: 16, margin: '0 0 8px 4px' }} />
                         </LightTooltip>
                       </div>
                       <div>
                         <span className="EMM-Score">{
-                          (parseInt(technicalPerformanceScore.dataPoints[0].valueX) < 0) ?
-                            <div className="performance-unavailable">Unavailable for this case</div>
-                            : technicalPerformanceScore.dataPoints[0].valueX
+                          (!hasHypotension) ?
+                            <span className="performance-unavailable">-</span>
+                            : <span>{hypotension.total}<sup className="Score-Unit">%</sup></span>
                         }</span>
-                        <span className="EMM-Score-Icon" style={{backgroundColor: 'rgba(255,125,125,.6)'}}>
-                          <Icon color="#FF4D4D" style={{marginTop:4}} path={mdiWater} size={'40px'} />
-                          <Icon color="#FFB1B1" className="Overlay" style={{marginTop:4}} path={mdiResistor} size={'26px'} />
+                        <span className="EMM-Score-Icon" style={{ backgroundColor: 'rgba(255,125,125,.6)' }}>
+                          <Icon color="#FF4D4D" style={{ marginTop: 4 }} path={mdiWater} size={'40px'} />
+                          <Icon color="#FFB1B1" className="Overlay" style={{ marginTop: 4 }} path={mdiResistor} size={'26px'} />
                         </span>
-                        
+
                       </div>
                     </Grid>
                     <Grid item xs={4} style={{ position: 'relative' }}>
-                      <div className="Section-Divider"></div>
+                      <LightTooltip interactive arrow title={hypothermia.body} placement="top" fontSize="small">
+                        <span className="Score-Speedometer">{this.getSpeedometer(hypothermia.description)}</span>
+                      </LightTooltip>
+                      <div className="Section-Divider" />
                       <div className="Section-Title">
-                        Hypothermia
-                        <LightTooltip interactive arrow title={EMM_DISTRACTION_TOOLTIP} placement="top" fontSize="small">
+                        {hypothermia.title}
+                        <LightTooltip interactive arrow title={<HorizontalBarChart dataPoints={hypothermia.dataPoints} title={hypothermia.subTitle}/>} placement="top" fontSize="small">
                           <InfoOutlinedIcon style={{ fontSize: 16, margin: '0 0 8px 4px' }} />
                         </LightTooltip>
                       </div>
                       <div>
                         <span className="EMM-Score">{
-                          (parseInt(technicalPerformanceScore.dataPoints[0].valueX) < 0) ?
-                            <span className="performance-unavailable">Unavailable for this case</span>
-                            : technicalPerformanceScore.dataPoints[0].valueX
+                          (!hasHypothermia) ?
+                            <span className="performance-unavailable">-</span>
+                            : <span>{hypothermia.total}<sup className="Score-Unit">%</sup></span>
                         }</span>
-                        <span className="EMM-Score-Icon" style={{backgroundColor: 'rgba(207, 185, 228,.6)'}}><Icon color="#A77ECD" path={mdiThermometerLow} size={'32px'} /></span>
+                        <span className="EMM-Score-Icon" style={{ backgroundColor: 'rgba(207, 185, 228,.6)' }}><Icon color="#A77ECD" path={mdiThermometerLow} size={'32px'} /></span>
                       </div>
                     </Grid>
                     <Grid item xs={4} style={{ position: 'relative' }}>
-                      <div className="Section-Divider"></div>
+                      <LightTooltip interactive arrow title={hypoxia.body} placement="top" fontSize="small">
+                        <span className="Score-Speedometer" style={{ marginRight:10 }}>{this.getSpeedometer(hypoxia.description)}</span>
+                      </LightTooltip>
+                      <div className="Section-Divider" />
                       <div className="Section-Title">
-                        Hypoxia
-                        <LightTooltip interactive arrow title={EMM_ADVERSEEVENT_TOOLTIP} placement="top" fontSize="small">
+                        {hypoxia.title}
+                        <LightTooltip interactive arrow title={<HorizontalBarChart dataPoints={hypoxia.dataPoints} title={hypoxia.subTitle}/>} placement="top" fontSize="small">
                           <InfoOutlinedIcon style={{ fontSize: 16, margin: '0 0 8px 4px' }} />
                         </LightTooltip>
                       </div>
                       <div>
                         <span className="EMM-Score">{
-                          (parseInt(technicalPerformanceScore.dataPoints[0].valueX) < 0) ?
-                            <span className="performance-unavailable">Unavailable for this case</span>
-                            : technicalPerformanceScore.dataPoints[0].valueX
+                          (!hasHypoxia) ?
+                            <span className="performance-unavailable">Not Available</span>
+                            : <span>{hypoxia.total}<sup className="Score-Unit">%</sup></span>
                         }</span>
-                        <span className="EMM-Score-Icon" style={{backgroundColor: 'rgba(200, 200, 200,.6)'}}><Icon color="#575757" path={mdiDivingScubaTank} size={'32px'} /></span>
+                        <span className="EMM-Score-Icon" style={{ backgroundColor: 'rgba(200, 200, 200,.6)' }}><Icon color="#575757" path={mdiDivingScubaTank} size={'32px'} /></span>
                       </div>
                     </Grid>
                   </Grid>
---
+++ app/containers/EMMReports/EMMOverview/HorizontalBarChart/HorizontalBarChart.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { Grid, withStyles, Tooltip } from '@material-ui/core';
+import C3Chart from 'react-c3js';
+import './style.scss';
+
+export default class HorizontalBarChart extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.chartRef = React.createRef();
+    this.id = `bar-chart-${this.props.id}`;
+    this.state = {
+      chartID: 'barChart',
+      chartData: {
+        data: {
+          x: 'x',
+          columns: [], //Dynamically populated
+          type: 'bar',
+          labels: {
+            show: true
+          },
+          colors: {
+            y: (d) => this.chooseColour(d)
+          },
+        }, // End data
+        bar: {
+          width: 24
+        },
+        axis: {
+          rotated: true,
+          x: {
+            show: true,
+            type: 'category'
+          },
+          y: {
+            show: false
+          }
+        },
+        grid: {
+          y: {
+            show: true
+          }
+        },
+        tooltip: {
+          show: false
+        },
+        legend: {
+          show: false
+        },
+        size: {
+          height: 258,
+          width: 300
+        },
+      }
+    }
+
+  };
+
+  componentDidUpdate(prevProps) {
+    if (!prevProps.dataPoints && this.props.dataPoints) {
+      this.generateChartData();
+    }
+  }
+
+  componentDidMount() {
+    this.generateChartData();
+  }
+
+  generateChartData() {
+    let { dataPoints } = this.props;
+    if (!dataPoints) {
+      return;
+    }
+
+    let formattedData = { x: ['x'], y: ['y'] };
+    let colours = [];
+    dataPoints.map((point, index) => {
+      formattedData.x.push(point.valueX);
+      colours.push(point.note)
+      formattedData.y.push(parseInt(point.valueY));
+    });
+    let chartData = this.state.chartData;
+    chartData.data.columns = [formattedData.x, formattedData.y];
+    let chart = this.chartRef.current && this.chartRef.current.chart;
+    chart && chart.load(chartData);
+
+    this.setState({ chartData, colours, isLoaded: true })
+  }
+
+  chooseColour(d) {
+    return this.state.colours[d.x - 1] || '#6EDE95';
+  }
+
+  render() {
+    return (
+      <Grid container spacing={1} className="Horizontal-BarChart" direction="column">
+        <Grid item xs style={{textAlign:'center'}}>
+          {this.props.title}
+        </Grid>
+        <Grid item xs>
+          <C3Chart className={this.state.chartID} ref={this.chartRef} {...this.state.chartData} />
+        </Grid>
+      </Grid>
+    );
+  }
+}
