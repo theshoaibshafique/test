c4b84f51b039cbbe98fccded4126a03b9a4b93e4
Marius De Allie <m.deallie@surgicalsafety.com>
2021-04-18 16:50:49 -0400

    https://surgicalsafety.atlassian.net/browse/IPE-116 Refactor LegendPagination component to render donut chart as a child component and move legend page change event handler methods from DonutChart component and into LegendPagination component.

--- app/components/LegendPagination/LegendPagination.js
+++ app/components/LegendPagination/LegendPagination.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { IconButton, Typography }from '@material-ui/core';
 import { ChevronLeft, ChevronRight, FirstPage, LastPage } from '@material-ui/icons';
@@ -20,47 +20,77 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const LegendPagination = (props) => {
-    const { page, pageCount, onPageIncrement, onPageDecrement, onPageStart, onPageEnd } = props;
+    const [page, setPage] = useState(1);
+    const [pageCount, setPageCount] = useState(1); 
+    const [itemsPerPage, setItemsPerPage] = useState(props.itemsPerPage)
+    const { legendData, children } = props;
+    
+    useEffect(() => {
+        // Calcuate number of legend pages and update pageCount piece of state.
+        setPageCount(Math.ceil(props.legendData.length / itemsPerPage));
+    }, [props.legendData.length]);
+
+    /*** LEGEND PAGE CHANGE EVENT HANDLER ***/
+    const onPageChange = (buttonName) => {
+        switch(buttonName) {
+            case 'first':
+                setPage(1);
+                break;
+            case 'last':
+                setPage(pageCount);
+                break;
+            case 'prev':
+            case 'next':
+                setPage(prevPage => buttonName === 'prev' ? prevPage - 1 : prevPage + 1);
+            default:
+                return;
+        }
+    };
+    
     const classes = useStyles();
-  
-      return (
-        <div className={classes.root}>
-          <IconButton
-              onClick={onPageStart}
-              aria-label="first page"
-              disabled={page === 1}
-              size="small"
-          >
-              <FirstPage />
-          </IconButton>
-          <IconButton
-              onClick={onPageDecrement}
-              aria-label="previous page"
-              disabled={page === 1}
-              size="small"
-          >
-              <ChevronLeft />
-          </IconButton>
-          <Typography variant="body1" className={classes.text}>
-              {`${page}/${pageCount}`}
-          </Typography>
-          <IconButton
-              onClick={onPageIncrement}
-              aria-label="next page"
-              disabled={page === pageCount}
-              size="small"
-          >
-              <ChevronRight />
-          </IconButton>
-          <IconButton
-              onClick={onPageEnd}
-              aria-label="last page"
-              disabled={page === pageCount}
-              size="small"
-          >
-              <LastPage />
-          </IconButton>
-        </div>
+    return (
+          <React.Fragment>
+            {children.slice((page - 1) * itemsPerPage, page * itemsPerPage)}
+            {pageCount > 1 && (
+                <div className={classes.root}>
+                    <IconButton
+                        onClick={() => onPageChange('first')}
+                        aria-label="first page"
+                        disabled={page === 1}
+                        size="small"
+                        >
+                        <FirstPage />
+                    </IconButton>
+                    <IconButton
+                        onClick={() => onPageChange('prev')}
+                        aria-label="previous page"
+                        disabled={page === 1}
+                        size="small"
+                        >
+                        <ChevronLeft />
+                    </IconButton>
+                    <Typography variant="body1" className={classes.text}>
+                        {`${page}/${pageCount}`}
+                    </Typography>
+                    <IconButton
+                        onClick={() => onPageChange('next')}
+                        aria-label="next page"
+                        disabled={page === pageCount}
+                        size="small"
+                        >
+                        <ChevronRight />
+                    </IconButton>
+                    <IconButton
+                        onClick={() => onPageChange('last')}
+                        aria-label="last page"
+                        disabled={page === pageCount}
+                        size="small"
+                        >
+                        <LastPage />
+                    </IconButton>
+                </div>
+            )}
+        </React.Fragment>
       );
 };
 
