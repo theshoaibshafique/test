1a5ef92be72bfc5fa750467b583c9d550b0ee049
Adam Lee <a.lee@surgicalsafety.com>
2021-07-02 11:48:38 -0400

    Remove logs for Prod

--- app/components/Logger/Logger.js
+++ app/components/Logger/Logger.js
@@ -48,7 +48,7 @@ export class Logger {
         this.exitLogs.forEach((log) => {
             this.manualAddLog(...log);
         })
-        console.log("with exit logs " + this.recentEvents);
+        // console.log("with exit logs " + this.recentEvents);
         this.sendLogs();
         this.exitLogs = [];
     }
@@ -63,7 +63,7 @@ export class Logger {
             }
         }
         if (this.recentEvents.length) {
-            console.log(this.recent)
+            // console.log(this.recent)
             this.postLogs(process.env.LOGGER_API, 'post', this.userToken, process.env.LOGGER_APP_ID, jsonBody);
             this.recentEvents = [];
         }
@@ -92,11 +92,11 @@ export class Logger {
         const length = this.recentEvents.length;
         //If they updated the last value - and it wasnt a clear -> we just update the log
         if (reducedEvents.includes(event) && length && this.recentEvents[length - 1].id == id && value) {
-            console.log("updated log", log)
+            // console.log("updated log", log)
             this.recentEvents.splice(length - 1, 1, log)
             return;
         }
-        console.log("added log", log)
+        // console.log("added log", log)
         this.recentEvents.push(log)
     }
 
@@ -125,19 +125,19 @@ export class Logger {
                 //If its the most recent event - we update it
                 const length = this.recentEvents.length;
                 if (length && this.recentEvents[length -1].id == id && value){
-                    console.log('updated log', log)
+                    // console.log('updated log', log)
                     this.recentEvents.splice(length - 1, 1, log)
                 }
                 return;
             }
-            console.log('added log', log)
+            // console.log('added log', log)
             this.recentEvents.push(log)
         }
 
     }
 
     connectListeners() {
-        console.log('connected');
+        // console.log('connected');
         events.forEach(event => {
             document.querySelectorAll(`.${classPrefix}${event}`).forEach(item => {
                 item.removeEventListener(event, this._addLog);
