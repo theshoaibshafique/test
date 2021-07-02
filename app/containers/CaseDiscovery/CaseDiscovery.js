57c55b79ef049bfcb2419f80bac09d23b9c71c65
Adam Lee <a.lee@surgicalsafety.com>
2021-07-02 11:46:54 -0400

    Update window for star click

--- app/containers/CaseDiscovery/CaseDiscovery.js
+++ app/containers/CaseDiscovery/CaseDiscovery.js
@@ -159,9 +159,11 @@ function Case(props) {
         <div className="title" >
           {procedureName}
         </div>
-        <div className={`save-toggle ${!isSaved && 'not-saved'}`}  onClick={(e) => { e.stopPropagation(); handleSaveCase() }}>
-          <IconButton style={{ marginTop:-6, marginBottom:-11 }} title={isSaved ? "Remove from saved cases" : "Save case"}>
-            {isSaved ? <StarIcon  style={{ color: '#EEDF58', fontSize:29  }}   /> : <StarBorderIcon style={{ color: '#828282', fontSize:29  }}  />}
+        <div >
+          <IconButton 
+            className={`save-toggle ${!isSaved && 'not-saved'}`} onClick={(e) => { e.stopPropagation(); handleSaveCase() }}
+            style={{ marginTop: -6, marginBottom: -11 }} title={isSaved ? "Remove from saved cases" : "Save case"}>
+            {isSaved ? <StarIcon style={{ color: '#EEDF58', fontSize: 29 }} /> : <StarBorderIcon style={{ color: '#828282', fontSize: 29 }} />}
           </IconButton>
 
         </div>
@@ -480,16 +482,16 @@ export default function CaseDiscovery(props) { // eslint-disable-line react/pref
 
     const fetchSavedCases = async () => {
       await globalFunctions.axiosFetch(process.env.CASE_DISCOVERY_API + "bookmarks", 'get', userToken, {})
-      .then(result => {
-        result = result.data;
-        setData({
-          'savedCases': result
-        })
-      }).catch((error) => {
-        console.log("uh no.")
-      }).finally(() => {
+        .then(result => {
+          result = result.data;
+          setData({
+            'savedCases': result
+          })
+        }).catch((error) => {
+          console.log("uh no.")
+        }).finally(() => {
 
-      });
+        });
     }
 
     fetchCases();
@@ -540,9 +542,9 @@ export default function CaseDiscovery(props) { // eslint-disable-line react/pref
 
   const handleSaveCase = async (caseId) => {
     const isSav = savedCases.includes(caseId);
-    await globalFunctions.axiosFetch(`${process.env.CASE_DISCOVERY_API}bookmarks?case_id=${caseId}&is_bookmarked=${!isSav}` , 'PUT', userToken, {})
+    await globalFunctions.axiosFetch(`${process.env.CASE_DISCOVERY_API}bookmarks?case_id=${caseId}&is_bookmarked=${!isSav}`, 'PUT', userToken, {})
       .then(result => {
-        logger && logger.manualAddLog('click', `${isSav ? 'remove' : 'add'}-saved-case`, {caseId:caseId});
+        logger && logger.manualAddLog('click', `${isSav ? 'remove' : 'add'}-saved-case`, { caseId: caseId });
         result = result.data;
         setData({
           'savedCases': result
@@ -1175,9 +1177,11 @@ function DetailedCase(props) {
           </div>
           <div className="case-header">
             <div className="case-title">{procedureTitle}</div>
-            <div className={`save-toggle ${!isSaved && 'not-saved'}`} onClick={(e) => { e.stopPropagation(); handleSaveCase() }}>
-              <IconButton style={{marginRight:55, marginTop:-12, marginBottom:-11}} title={isSaved ? "Remove from saved cases" : "Save case"}>
-                {isSaved ? <StarIcon  style={{ color: '#EEDF58', fontSize:36  }}  /> : <StarBorderIcon style={{ color: '#828282', fontSize:36  }}  />}
+            <div >
+              <IconButton
+                className={`save-toggle ${!isSaved && 'not-saved'}`} onClick={(e) => { e.stopPropagation(); handleSaveCase() }}
+                style={{ marginRight: 55, marginTop: -12, marginBottom: -11 }} title={isSaved ? "Remove from saved cases" : "Save case"}>
+                {isSaved ? <StarIcon style={{ color: '#EEDF58', fontSize: 36 }} /> : <StarBorderIcon style={{ color: '#828282', fontSize: 36 }} />}
               </IconButton>
             </div>
           </div>
