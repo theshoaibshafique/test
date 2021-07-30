f4ce6f86bc0facd31f3c6c60073f910c5bc28636
Marius De Allie <m.deallie@surgicalsafety.com>
2021-07-30 11:00:42 -0400

    IPE-187-Update itemsPerPage prop value passed to LegendPagination component from 10 to 7 within DonutChart component.

--- app/components/Report/DonutChart/DonutChart.js
+++ app/components/Report/DonutChart/DonutChart.js
@@ -131,7 +131,7 @@ export default class DonutChart extends React.PureComponent {
       <div className={`${this.state.chartID} donut-chart-detailed-legend`}>
         <LegendPagination
           legendData={this.state.legendData}
-          itemsPerPage={10}
+          itemsPerPage={7}
         >
           {this.state.legendData && this.state.legendData.map(([id, value], index) => {
             if (id == "NA") {
