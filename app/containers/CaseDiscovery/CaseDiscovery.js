d062256068075a31fad2b345bf0e2296bf616f7c
Adam Lee <a.lee@surgicalsafety.com>
2021-08-13 10:19:11 -0400

    https://surgicalsafety.atlassian.net/browse/IPE-201 Only show procedure Time when valid

--- app/containers/CaseDiscovery/CaseDiscovery.js
+++ app/containers/CaseDiscovery/CaseDiscovery.js
@@ -1601,7 +1601,9 @@ function ProcedureDistribution(props) {
   const range = [duration, ...globalFunctions.range(Math.max(0, mu - 4.5 * sigma), mu + 4.5 * sigma, sigma / 20)].sort();
   // console.log(range)
   const chartRef = useRef(null);
-
+  if (!shape || !scale){
+    return <div></div>
+  }
 
   const createCustomTooltip = (d, defaultTitleFormat, defaultValueFormat, color) => {
     d = d[0]
@@ -1649,9 +1651,9 @@ function ProcedureDistribution(props) {
       }
     },
     grid: {
-      x: {
+      x: duration > 0 ? {
         lines: [{ "value": duration, "text": globalFunctions.formatSecsToTime(duration, true, true), "class": "marker" }]
-      },
+      } : {},
     },
     legend: {
       show: false
