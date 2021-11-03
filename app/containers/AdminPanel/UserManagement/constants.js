df1abbacb6ed69f2c31e5832d9734462b05be900
Adam Lee <a.lee@surgicalsafety.com>
2021-11-03 09:35:57 -0400

    Update UM Learn more verbage

--- app/containers/AdminPanel/UserManagement/constants.js
+++ app/containers/AdminPanel/UserManagement/constants.js
@@ -8,21 +8,21 @@ export const LEARNMORE_INFO = {
         order: 0,
         content: {
             "Full Access": "Has access to view all <facility> analytics. Users with this role will also be able to update the <facility> product configurations, within the settings page.",
-            "View Only": "Has access to view Efficiency analytics. Users with this role can be given access to all <facility> analytics, or can be restricted to specific departments or rooms."
+            "View Only": "Has access to view Efficiency analytics. Users with this role can be given access to all <facility> analytics, or can be restricted to specific rooms."
         }
     },
     "eM&M": {
         order: 1,
         content: {
             "Full Access": "Has access to view all <facility>’s Enhanced M&M reports. Users with this role will also be able to request new Enhanced M&M reports, and present reports without security enabled, which allows sharing via video conference tools.",
-            "View Only": "Has access to view <facility>’s Enhanced M&M reports. Users with this role can be given access to all <facility>’s Enhanced M&M reports, or can be restricted to specific departments or rooms."
+            "View Only": "Has access to view <facility>’s Enhanced M&M reports. Users with this role can be given access to all <facility>’s Enhanced M&M reports, or can be restricted to specific rooms."
         }
     },
     "Case Discovery": {
         order: 2,
         content: {
             "Full Access": "Has access to view all <facility> cases within Case Discovery. Users with this role will also be able to flag eligible cases, update what information is requested when a user flags a case, and present flag video clips without security enabled, which allows sharing via video conference tools.",
-            "View Only": "Has access to view <facility> cases within Case Discovery. Users with this role can be given access to all <facility> cases, or can be restricted to specific departments or rooms."
+            "View Only": "Has access to view <facility> cases within Case Discovery. Users with this role can be given access to all <facility> cases, or can be restricted to specific rooms."
         }
 
     },
@@ -30,7 +30,7 @@ export const LEARNMORE_INFO = {
         order: 3,
         content: {
             "Full Access": "Has access to view all <facility> Surgical Safety Checklist analytics. Users with this role will also be able to update the <facility> product configurations, within the settings page.",
-            "View Only": "Has access to view <facility> Surgical Safety Checklist analytics. Users with this role can be given access to all <facility> analytics, or can be restricted to specific departments or rooms."
+            "View Only": "Has access to view <facility> Surgical Safety Checklist analytics. Users with this role can be given access to all <facility> analytics, or can be restricted to specific rooms."
         }
     },
 }
