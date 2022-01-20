c46c8d3880f4dd6bb3a276bddce4c453ea2f8a40
r.tsang <r.tsang@surgicalsafety.com>
2022-01-20 12:36:25 -0500

    load remote facility graphic image

--- app/containers/Welcome/Welcome.js
+++ app/containers/Welcome/Welcome.js
@@ -7,27 +7,30 @@
 import React from 'react';
 import './style.scss';
 import { Grid } from '@material-ui/core';
-import facilityGraphic from './img/Surgeons_Image_Placeholder.png';
 
 export default class Welcome extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
     super(props);
-
+    this.ref = React.createRef();
   }
 
   componentDidMount() {
-
-  };
+  }
 
   render() {
+    const imgSrc = 'https://api.insights.surgicalsafety.com/media/default.png';
     return (
       <div className="welcome-page">
-        <div className="facility-graphic-container"></div>
+        <div className="facility-graphic-container">
+          <img src={imgSrc} ref={this.ref}/>
+        </div>
         <Grid item xs className="welcome-message">
           <div className="welcome">Welcome</div>
           <div className="personal-name">{this.props.firstName} {this.props.lastName}</div>
         </Grid>
-        <div className="footer subtle-subtext">Can’t find what you’re looking for? Contact your administrator for assistance.</div>
+        <div className="footer subtle-subtext">Can’t find what you’re looking for? Contact your administrator for
+          assistance.
+        </div>
       </div>
     );
   }
