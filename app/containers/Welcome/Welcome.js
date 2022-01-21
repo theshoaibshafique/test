c985e8f1f83a89c5bd69dea5d9ab10b00b175f88
r.tsang <r.tsang@surgicalsafety.com>
2022-01-21 13:13:46 -0500

    implement the animation of dashboard graphic

--- app/containers/Welcome/Welcome.js
+++ app/containers/Welcome/Welcome.js
@@ -6,28 +6,101 @@
 
 import React from 'react';
 import './style.scss';
-import { Grid } from '@material-ui/core';
+import { motion } from 'framer-motion';
 
 export default class Welcome extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+
   constructor(props) {
     super(props);
     this.ref = React.createRef();
+    this.state = {
+      animate: true
+    };
   }
 
   componentDidMount() {
+
   }
 
   render() {
     const imgSrc = 'https://api.insights.surgicalsafety.com/media/default.png';
     return (
       <div className="welcome-page">
-        <div className="facility-graphic-container">
+        <motion.div
+          className="facility-graphic-container"
+          style={{visibility: this.state.animate ? 'visible' : 'hidden' }}
+          animate={{
+            y: '-100%',
+          }}
+          transition={{
+            delay: 2,
+            duration: 2,
+          }}
+        >
+          <div>
+            <img src={imgSrc} ref={this.ref}/>
+          </div>
+        </motion.div>
+        <motion.div
+          className="facility-graphic-container"
+          style={{visibility: this.state.animate ? 'visible' : 'hidden' }}
+          animate={{
+            y: '-100%',
+          }}
+          transition={{
+            delay: 2,
+            duration: 2,
+          }}
+        >
+          <div>
+            <img src={imgSrc} ref={this.ref}/>
+          </div>
+        </motion.div>
+        <div
+          style={{visibility: 'hidden' }}
+          className="facility-graphic-container"
+        >
           <img src={imgSrc} ref={this.ref}/>
         </div>
-        <Grid item xs className="welcome-message">
+
+        <motion.div
+          className="welcome-message"
+          style={{visibility: this.state.animate ? 'visible' : 'hidden' }}
+          initial={{ opacity: 1 }}
+          animate={{
+            y: '-100%',
+            opacity: 0
+          }}
+          transition={{
+            duration: 2,
+          }}
+        >
           <div className="welcome">Welcome</div>
           <div className="personal-name">{this.props.firstName} {this.props.lastName}</div>
-        </Grid>
+        </motion.div>
+        <motion.div
+          className="welcome-message"
+          style={{visibility: this.state.animate ? 'visible' : 'hidden' }}
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1
+          }}
+          transition={{
+            delay: 4,
+            duration: 2,
+          }}
+        >
+          <div className="welcome">Welcome</div>
+          <div className="personal-name">{this.props.firstName} {this.props.lastName}</div>
+        </motion.div>
+
+        <div className="facility-graphic-container" style={{visibility: !this.state.animate ? 'visible' : 'hidden' }}>
+          <img src={imgSrc} ref={this.ref}/>
+        </div>
+        <div className="welcome-message" style={{visibility: !this.state.animate ? 'visible' : 'hidden' }}>
+          <div className="welcome">Welcome</div>
+          <div className="personal-name">{this.props.firstName} {this.props.lastName}</div>
+        </div>
         <div className="footer subtle-subtext">Can’t find what you’re looking for? Contact your administrator for
           assistance.
         </div>
