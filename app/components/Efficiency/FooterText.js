0de10b25d0189f73c84a000a29094a03357aa03a
Adam Lee <a.lee@surgicalsafety.com>
2022-03-03 18:13:19 -0500

    fix footer formatting

--- app/components/Efficiency/FooterText.js
+++ app/components/Efficiency/FooterText.js
@@ -2,7 +2,7 @@ import React from 'react';
 import globalFunctions from '../../utils/global-functions';
 
 const FooterText = ({ days, facilityName, fcotsThreshold, otsThreshold, turnoverThreshold }) => {
-  const [hh, mm, ss] = globalFunctions.formatSecsToTime(turnoverThreshold ?? 0)?.split(':');
+  // const [hh, mm, ss] = globalFunctions.formatSecsToTime(turnoverThreshold ?? 0);
   return (
     <div>
       {days >= 0 && days !== null && <i>Based on <strong>{days} days</strong> with elective hours from your filter criteria <br />
@@ -11,14 +11,14 @@ const FooterText = ({ days, facilityName, fcotsThreshold, otsThreshold, turnover
       {facilityName && !isNaN(fcotsThreshold) && (
         <div>
           <i>
-            On-time start grace period for <strong>{facilityName}</strong> is <strong>{fcotsThreshold}</strong> min for first cases of block, and <strong>{otsThreshold}</strong> min for all other cases
+            On-time start grace period for <strong>{facilityName}</strong> is <strong>{globalFunctions.formatSecsToTime(fcotsThreshold, true, true)}</strong> for first cases of block, and <strong>{globalFunctions.formatSecsToTime(otsThreshold, true, true)}</strong> for all other cases
           </i>
         </div>
       )}
       {facilityName && !isNaN(turnoverThreshold) && (
         <div>
           <i>
-            Turnovers longer than <strong>{facilityName}</strong> cutoff threshold of <strong>{hh}</strong> hr <strong>{mm}</strong> min were ommited from analysis
+            Turnovers longer than <strong>{facilityName}</strong> cutoff threshold of <strong>{globalFunctions.formatSecsToTime(turnoverThreshold ?? 0, true, true)}</strong> were ommited from analysis
           </i>
         </div>
       )}
