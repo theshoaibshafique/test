4ffcd1471a71d585190f4e2afc25f29ea33b21ac
Adam Lee <a.lee@surgicalsafety.com>
2022-03-03 10:11:34 -0500

    Add No data view for Trends

--- app/components/Charts/LineGraph.js
+++ app/components/Charts/LineGraph.js
@@ -4,7 +4,14 @@ import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, ReferenceA
 import { axisLabelStyle, axisStyles } from './styles';
 
 const equalProps = (props, prevProps) => props === prevProps;
-
+const NoDataOverlay = (props) => (
+  <div style={{
+    position: 'absolute', top: '40%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: '#828282', width: '100%', height: '100%'
+  }} className='header-1 flex vertical-center'>No Data</div>
+)
 /*
 * @TODO: Possibly move this to a new component if a universal tooltip design can be decided on. Also, decide on colours / format
 */
@@ -13,7 +20,7 @@ const CustomTooltip = ({ active, payload }) => {
     const [{ payload: { date, percentage } }] = payload;
     return (
       <div
-        style={{background:'#F2F2F2', borderRadius:4, padding:8}}
+        style={{ background: '#F2F2F2', borderRadius: 4, padding: 8 }}
       >
         <div>Date: <span className='bold'>{moment(date).format('MMM D YYYY')}</span></div>
         <div>Percentage: <span className='bold'>{percentage}</span></div>
@@ -35,21 +42,26 @@ const LineGraph = React.memo(({
   data, xAxisLabel, yAxisLabel, interval, xTickSize, xTickMargin, areaReference
 }) => {
   const formatTick = (item) => moment(item).format('MMM');
-  const [start,end] = areaReference ?? []
+  const [start, end] = areaReference ?? []
+  const hasData = data?.length;
+  data = hasData ? data : [{ date: '' }]
   return (
-    <ResponsiveContainer width="100%" height={200}>
-      <LineChart
-        data={data}
-      >
-        <ReferenceArea x1={start} x2={end} fill="#B3CAD3" fillOpacity={.2} alwaysShow/>
-        <XAxis scale='linear' dataKey="date" label={{ ...xAxisLabel, ...axisLabelStyle }} style={axisStyles} interval={interval} tickSize={xTickSize} tickMargin={xTickMargin} tickFormatter={formatTick} />
-        <YAxis dataKey="percentage" label={{ ...yAxisLabel, ...axisLabelStyle }} style={axisStyles} />
-        <Line type="monotone" dataKey="percentage" stroke="#028CC8" dot={false} />
-        <Line type="monotone" dataKey="network" stroke="#e0e0e0" dot={false} />
-        <Tooltip content={<CustomTooltip />} />
-        
-      </LineChart>
-    </ResponsiveContainer>
+    <div style={{ position: 'relative' }}>
+      <ResponsiveContainer width="100%" height={200}>
+        <LineChart
+          data={data}
+        >
+          <ReferenceArea x1={start} x2={end} fill="#B3CAD3" fillOpacity={.2} alwaysShow />
+          <XAxis scale='linear' dataKey="date" label={{ ...xAxisLabel, ...axisLabelStyle }} style={axisStyles} interval={interval} tickSize={xTickSize} tickMargin={xTickMargin} tickFormatter={formatTick} />
+          <YAxis dataKey="percentage" label={{ ...yAxisLabel, ...axisLabelStyle }} style={axisStyles} />
+          <Line type="monotone" dataKey="percentage" stroke="#028CC8" dot={false} />
+          <Line type="monotone" dataKey="network" stroke="#e0e0e0" dot={false} />
+          <Tooltip content={<CustomTooltip />} />
+
+        </LineChart>
+      </ResponsiveContainer>
+      {!hasData && <NoDataOverlay />}
+    </div>
   );
 }, equalProps);
 
