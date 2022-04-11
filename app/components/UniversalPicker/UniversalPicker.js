f7622d7ac062139546fb3c767a7c90004f6f9711
Pavel Gusilic <p.gusilic@surgicalsafety.com>
2022-04-11 10:12:37 -0400

    removed span

--- app/components/UniversalPicker/UniversalPicker.js
+++ app/components/UniversalPicker/UniversalPicker.js
@@ -194,7 +194,6 @@ class UniversalPicker extends React.Component {
     const defaultThreshold = globalFunctions.formatSecsToTime(this.props.defaultThreshold, true, true).trim() || "0 min";
     return (
       <Grid container spacing={1} justify="center" className="universal-picker">
-        <span style={{ display: 'flex', alignItems: 'center', marginRight: 16 }}></span>
         {this.state.showDate && <Grid item xs={1} style={{ minWidth: 258 }}>
           <CustomDateRangePicker 
             dateLabel={this.state.selectedDate} 
